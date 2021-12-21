80ec6dd1a149ca018db4cdd9f620327b5fc2fdd2
Kazh-boop <baptiste.lelievre56@gmail.com>
2021-12-21 11:42:26 +0100

    test simples et fonctionnels

---
+++ test/TestSave.java
@@ -0,0 +1,63 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Familiar;
+import app.model.SaveManager;
+
+public class TestSave {
+	
+	private Familiar saveFamiliarCat, saveFamiliarDog;
+	private SaveManager saveManager;
+	
+	private static String nameSaveCat, nameSaveDog;
+	private static final String ABSOLUTEPATH = "save/";
+	private static final String EXTENSION = ".dat";
+
+
+	@Before
+	public void setUp() throws Exception {
+		saveManager = new SaveManager(ABSOLUTEPATH);
+		
+		// Cat save
+		saveFamiliarCat = new Cat("Filou");
+		nameSaveCat = saveFamiliarCat.getUID();
+		
+		// Dog save
+		saveFamiliarDog = new Dog("Toutou");
+		nameSaveDog = saveFamiliarDog.getUID();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		saveManager.deleteSave(nameSaveCat);
+		saveManager.deleteSave(nameSaveDog);
+		saveFamiliarCat = saveFamiliarDog = null;
+		saveManager = null;
+	}
+
+	@Test
+	public void saveFamiliar() throws IOException, ClassNotFoundException {
+		// Cat
+		saveManager.openFile(nameSaveCat); // ouverture du fichier
+		saveManager.writeSave(saveFamiliarCat); // ecriture des infos de saveFamiliar dans TestSave.dat
+		
+		// Dog
+		saveManager.openFile(nameSaveDog);
+		saveManager.writeSave(saveFamiliarDog);
+		
+		
+		assertEquals(true, new File(ABSOLUTEPATH+nameSaveCat+EXTENSION).exists()); // Check Cat
+		assertEquals(true, new File(ABSOLUTEPATH+nameSaveDog+EXTENSION).exists()); // Check Dog
+	}
+
+}
