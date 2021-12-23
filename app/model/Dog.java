86778a5facbad38f4f0f21d021b7fecd60fde6f8
bleyzer <guilhem.mazoyer@laposte.net>
2021-12-23 11:19:49 +0100

    Correction de la nourriture dans la class Dog.java et de son nom de type associé

--- app/model/Dog.java
+++ app/model/Dog.java
@@ -5,12 +5,12 @@ public class Dog extends Familiar {
     public Dog(String name) {
         super();
         this.name = name;
-        this.food = "Patée";
-        this.familiarType = "Chat";
+        this.food = "Croquette";
+        this.familiarType = "Chien";
     }
 
     public Dog(Dog dog) {
         super(dog);
-        this.food = "Patée";
+        this.food = "Croquette";
     }
 }
